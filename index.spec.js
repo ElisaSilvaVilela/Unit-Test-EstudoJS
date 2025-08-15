981f549b6c248c0c47271eb92b7e1c51de33e400
Elisa Vilela <vilelaelisa2517@gmail.com>
2025-08-15 16:50:41 -0300

    Add another failing test

--- index.spec.js
+++ index.spec.js
@@ -6,3 +6,14 @@ it ('se a lista estiver vazia, a comissao é zero', () => {
 
     expect(resultadoAtual).toBe(resultadoEsperado);
 });
+
+it ('calcula comissão quando só tem um item na lista', () => {
+    const resultadoAtual = calculaComissaoVenda([{
+        id: 'PROD-123',
+        precoUnitario: 100,
+        quantidadeVendida: 1
+    }])
+    const resultadoEsperado = 50;
+
+    expect(resultadoAtual).toBe(resultadoEsperado);
+})
