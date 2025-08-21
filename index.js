e53bb2a71ce2deb9e0d9644755d84069f798554a
Elisa Vilela <vilelaelisa2517@gmail.com>
2025-08-21 16:59:49 -0300

    Add special comission feature

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 module.exports = function(items) {
+     let foundSpecialProduct = false;
      const totalPrice = items.map(item => {
-             const { precoUnitario, quantidadeVendida } = item;
+             const { id, precoUnitario, quantidadeVendida } = item;
+             if (id === 'XP-0101'){
+                foundSpecialProduct = true;
+             }
              return precoUnitario * quantidadeVendida;
         }).reduce((a, b) => a + b, 0);
-
+        if(foundSpecialProduct) return totalPrice * 0.2;
         if(totalPrice < 1999.99) return totalPrice * 0.05;
         if(totalPrice > 1999.99 && totalPrice < 4999.99) return totalPrice * 0.1;
         if(totalPrice > 4999.99) return totalPrice * 0.15;
