12fedb5c3105c13b8f9ff7907094ad511b01406e
Elisa Vilela <vilelaelisa2517@gmail.com>
2025-08-21 15:44:30 -0300

    Refactor code a bit

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 module.exports = function(items) {
     if (!items.length) return 0;
-    if (items.length === 1) {
-    const { precoUnitario, quantidadeVendida } = items[0];
-    return precoUnitario * quantidadeVendida * 0.05;  
-    }
-    if (items.length > 1) {
-        const prices = items.map(item => {
+     const prices = items.map(item => {
              const { precoUnitario, quantidadeVendida } = item;
              return precoUnitario * quantidadeVendida;
         });
@@ -13,5 +8,4 @@ module.exports = function(items) {
         const totalPrice = prices.reduce((a, b) => a + b);
 
         return totalPrice * 0.05;
-    }
-}
+    };   
