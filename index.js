330c63f001a2447891935a1c8325e7088340798a
Elisa Vilela <vilelaelisa2517@gmail.com>
2025-09-03 15:14:43 -0300

    Remove duplicated code by adding a multiply function

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ module.exports = function(items) {
      const totalPrice = items.map(item => {
              const { id, precoUnitario, quantidadeVendida } = item;
              if (id === 'XP-0101')foundSpecialProduct = true;     
-             return precoUnitario * quantidadeVendida;
+             return multiply(precoUnitario, quantidadeVendida);
         }).reduce((a, b) => a + b, 0);
         if(foundSpecialProduct) return totalPrice * 0.2;
-        if(totalPrice < 1999.99) return totalPrice * 0.05;
-        if(totalPrice > 1999.99 && totalPrice < 4999.99) return totalPrice * 0.1;
-        if(totalPrice > 4999.99) return totalPrice * 0.15;
+        if(totalPrice < 1999.99) return multiply(totalPrice, 0.05);
+        if(totalPrice > 1999.99 && totalPrice < 4999.99) return multiply(totalPrice, 0.1);
+        if(totalPrice > 4999.99) return multiply(totalPrice, 0.15);
     };   
+
+    function multiply(val1, val2){
+     return val1 * val2;
+    };
